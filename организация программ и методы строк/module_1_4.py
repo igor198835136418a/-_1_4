98f272154dda8149e01eccfec6be748ee7b05f2a
igor19881 <34863818+igor198835136418a@users.noreply.github.com>
2024-12-16 16:32:22 +0700

    Add files via upload

---
+++ организация программ и методы строк/module_1_4.py
@@ -0,0 +1,18 @@
+my_string = input("Сколько символов в строке?")
+my_string = "Сколько символов в строке?"
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
+
+
+
+
+
